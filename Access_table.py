024cfaa24b479d1bee0de1d53275345e132c2c39
sriya-kotta <85950954+sriya-kotta@users.noreply.github.com>
2021-06-29 14:53:12 -0700

    Add files via upload

---
+++ Access_table.py
@@ -0,0 +1,27 @@
+
+import re
+import pandas as pd
+def datatable():
+    ip_pattern = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
+    ip = []
+    date_pattern = re.compile(r'(\d{2}/[A-z]{3}/\d{4}\:\d{2}\:\d{2}\:\d{2})')
+    date = []
+    parameter = []
+    campaign_id = []
+    with open(r'C:\Users\knkb\Desktop\access.txt','r') as file:
+        access = file.readlines()
+    for line in access:
+        ip.append(ip_pattern.search(line)[0])
+        date.append(date_pattern.search(line)[0])
+        if line.find("?") == -1:
+            parameter.append('none')
+            campaign_id.append([])
+        else:
+            parameter.append(re.findall('[a-zA-Z]+\=[a-zA-Z\d+\-\%\_\:]+',line))
+            campaign_id.append(re.findall(r'[c]\=(\d+)',line))
+    df = pd.DataFrame({'IP Address': ip, 'Date' : date, 'Parameter' : parameter, 'Campaign ID' : campaign_id})
+    df.to_csv('items.csv', index=False, encoding='utf-8')
+    pd.set_option('display.max_rows', 500)
+    pd.set_option('display.max_colwidth', 1000)
+    print(df)
+datatable()
